e77adf97d8a1c9b01cc88e9987442d0af3b08d3e
Edmond Mbadu <mbadungoma@gmail.com>
2024-03-02 13:55:29 -0800

    small text change

--- src/app/blogs/blogs/blogs.component.ts
+++ src/app/blogs/blogs/blogs.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-blogs',
   templateUrl: './blogs.component.html',
   styleUrl: './blogs.component.css',
 })
-export class BlogsComponent {}
+export class BlogsComponent implements OnInit {
+  ngOnInit(): void {
+    window.scroll(0, 0);
+  }
+}
