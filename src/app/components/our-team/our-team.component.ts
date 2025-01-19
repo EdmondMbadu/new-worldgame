cd4c8ef51db1fbd4a0736b23e21be0d609f39537
Edmond Mbadu <mbadungoma@gmail.com>
2025-01-19 13:12:56 -0800

    correct typo on team's page

--- src/app/components/our-team/our-team.component.ts
+++ src/app/components/our-team/our-team.component.ts
@@ -31,7 +31,7 @@ export class OurTeamComponent implements OnInit {
       name: 'Edmond Mbadu',
       title: 'Lead Developer, NewWorld Game Designer',
       description:
-        'Edmond is is the lead software developer who focuses on building scalable solutions, supporting the team, and delivering reliable software systems.',
+        'Edmond is  the lead software developer who focuses on building scalable solutions, supporting the team, and delivering reliable software systems.',
 
       profilePicPath:
         'https://firebasestorage.googleapis.com/v0/b/new-worldgame.appspot.com/o/avatar%2Flhf54MEukyMpSfk18xVBpWs1FCH3-ed-avatar.png?alt=media&token=cdfaa91c-5e77-4a3c-a49c-8d7b8a2f5a7d',
@@ -40,7 +40,7 @@ export class OurTeamComponent implements OnInit {
       name: 'Rain White',
       title: 'UI/UX Designer, Software Developer',
       description:
-        'Rain is a UI/UX designer and software developer who works on creating user-friendly designs and building functional, reliable software solutions.',
+        'Rain is the UI/UX designer and software developer who works on creating user-friendly designs and building functional, reliable software solutions.',
 
       profilePicPath:
         'https://firebasestorage.googleapis.com/v0/b/new-worldgame.appspot.com/o/team%2Frain-white.jpg?alt=media&token=4534a5d1-3bbc-4727-99ea-edff28e6321b',
