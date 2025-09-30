eb4d518a214a5d295ee6de0a1a0117e4cad7910c
Edmond Mbadu <mbadungoma@gmail.com>
2025-09-29 22:41:00 -0700

    updated the UI to make remove some space

--- src/app/blogs/careers-social/careers-social.component.ts
+++ src/app/blogs/careers-social/careers-social.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { SolutionService } from 'src/app/services/solution.service';
 
 @Component({
   selector: 'app-careers-social',
@@ -15,7 +17,13 @@ export class CareersSocialComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    window.scrollTo(0, 0);
+    window.scroll(0, 0);
+  }
+  isLoggedIn: boolean = false;
+  constructor(public auth: AuthService, private solution: SolutionService) {
+    this.auth.getCurrentUserPromise().then((user) => {
+      this.isLoggedIn = !!user;
+    });
   }
 
   async copyEmail() {
