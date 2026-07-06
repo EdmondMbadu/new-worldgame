035568bded1389a65dc79de21e5b9686748da22d
Edmond Mbadu <mbadungoma@gmail.com>
2026-07-06 09:15:31 -0700

    added different pages on the list

--- src/app/blogs/solution-a-thon-hubs/solution-a-thon-hubs.component.ts
+++ src/app/blogs/solution-a-thon-hubs/solution-a-thon-hubs.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-solution-a-thon-hubs',
   templateUrl: './solution-a-thon-hubs.component.html',
+  styleUrls: ['../solution-a-thon/solution-a-thon.component.css'],
 })
 export class SolutionAThonHubsComponent {}
--- src/app/blogs/solution-a-thon-sponsors/solution-a-thon-sponsors.component.ts
+++ src/app/blogs/solution-a-thon-sponsors/solution-a-thon-sponsors.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-solution-a-thon-sponsors',
   templateUrl: './solution-a-thon-sponsors.component.html',
+  styleUrls: ['../solution-a-thon/solution-a-thon.component.css'],
 })
 export class SolutionAThonSponsorsComponent {}
